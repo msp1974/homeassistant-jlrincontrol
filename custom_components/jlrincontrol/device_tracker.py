2b771c01f17d05c480c1939965a2285749601079
msp1974 <msparker@sky.com>
2020-04-24 20:14:18 +0100

    Added attrs to device tracker

--- custom_components/jlrincontrol/device_tracker.py
+++ custom_components/jlrincontrol/device_tracker.py
@@ -38,18 +38,27 @@ class JLRDeviceTracker:
     async def see_vehicle(self):
         """Update the device info."""
         dev_id = slugify(self._name)
-        p = self._vehicle.position
-        gps = [
-            round(p.get("position").get("latitude"), 8),
-            round(p.get("position").get("longitude"), 8),
-        ]
+        p = self._vehicle.position.get("position")
 
-        _LOGGER.debug("Updating {} Device Tracker".format(self._name))
+        if p:
+            gps = [
+                round(p.get("latitude"), 8),
+                round(p.get("longitude"), 8),
+            ]
 
-        await self._see(
-            dev_id=dev_id,
-            host_name=self._name,
-            source_type=SOURCE_TYPE_GPS,
-            gps=gps,
-            icon="mdi:car",
-        )
+            attrs = {}
+            loc_name = self._data.connection.reverse_geocode(gps[0], gps[1])
+            attrs["location"] = loc_name.get("formattedAddress")
+            attrs["speed"] = p.get("speed")
+            attrs["heading"] = p.get("heading")
+
+            _LOGGER.debug("Updating {} Device Tracker".format(self._name))
+
+            await self._see(
+                dev_id=dev_id,
+                host_name=self._name,
+                source_type=SOURCE_TYPE_GPS,
+                gps=gps,
+                icon="mdi:car",
+                attributes=attrs,
+            )
