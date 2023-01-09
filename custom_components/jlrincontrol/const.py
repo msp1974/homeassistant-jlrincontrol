d4315cc8c1f85470dd7d23fa8f8f5bde03c53f1e
Ismar Slomic <ismar@slomic.no>
2023-01-09 15:12:54 +0100

    feature: bump to v2.2.3

--- custom_components/jlrincontrol/const.py
+++ custom_components/jlrincontrol/const.py
@@ -14,7 +14,7 @@ _LOGGER = logging.getLogger(__name__)
 DOMAIN = "jlrincontrol"
 DATA_JLR_CONFIG = "jlrincontrol_config"
 JLR_DATA = "jlr_data"
-VERSION = "2.2.2"
+VERSION = "2.2.3"
 
 CONF_USE_CHINA_SERVERS = "use_china_servers"
 
